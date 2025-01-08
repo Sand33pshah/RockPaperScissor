0bd628afb5f2dd6fb814be04cd881410f35d1e7c
Sandip Shah <shahsandeep980@gmail.com>
2025-01-08 10:01:11 +0530

    added some styling

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function gamePlay(element){
     let currUserChoice = element.innerHTML;
-
-    document.getElementById('emojiUser').innerHTML = currUserChoice;
+    console.log(currUserChoice);
 
     // Generate a random choice for the computer 
-    const choices = ['&#x1F590', '&#x270C', '&#x270A']; const computerChoice = choices[Math.floor(Math.random() * choices.length)]; 
-    // Update the emojiComputer paragraph with the computer's choice
-    document.getElementById('emojiComputer').innerHTML = computerChoice; 
+    const choices = ['&#x1F590', '&#x270C', '&#x270A']; 
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    console.log(computerChoice);
+
+    if(currUserChoice === computerChoice){
+        console.log('Its a tie');
+    }
+
+    document.getElementById('emojiUser').innerHTML = currUserChoice;
+    document.getElementById('emojiComputer').innerHTML = computerChoice;
+
+
+
     // You can add more logic here to determine the winner and update the score
 }
